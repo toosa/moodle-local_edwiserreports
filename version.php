c9acd60661610deeb918ed737bcdd4313b7032ed
Prihantoosa <pht854@gmail.com>
2022-07-25 17:23:03 +0700

    1.3.0 ke 1.3.1

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_edwiserreports';
-$plugin->release   = '1.3.0';
-$plugin->version   = 2022043000;
+$plugin->release   = '1.3.1';
+$plugin->version   = 2022043001;
 $plugin->requires  = 2018120300;
 $plugin->maturity  = MATURITY_STABLE;
